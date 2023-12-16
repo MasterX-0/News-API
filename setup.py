04b1f71b5cc9e964dbc6c974d45cdc9df3a49017
Master X <117908721+MasterX-0@users.noreply.github.com>
2023-12-16 12:26:30 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
         'License :: OSI Approved :: GPL-3.0 License',
         'Operating System :: OS Independent',
     ],
-    author='Ishan Oshada',
+    author='MasterX',
     description='This is a free and versatile news service that provides users with real-time updates and a comprehensive range of news content. This API offers JSON results, ensuring easy integration into your applications or websites. Stay updated with the latest news from various sources, including popular news outlets like Hiru News and Adaderana News.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
