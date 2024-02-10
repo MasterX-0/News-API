1b5b502711242b4ed80d9267ea11b517dd93ea64
MasterX <117908721+MasterX-0@users.noreply.github.com>
2024-02-11 00:14:28 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='MNewsAPI',
-    version='0.1.0',
+    name='NewsAPI',
+    version='0.1.1',
     packages=find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3',
